d62aa2c0f5de95c416dc37a8182a7ddefc788819
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2013-08-28 18:40:06 +0200

    make TemplateIsoparameteric independent of particular matrix and vector types

--- FemLib/FiniteElement/TemplateIsoparametric.h
+++ FemLib/FiniteElement/TemplateIsoparametric.h
@@ -28,20 +28,35 @@ namespace FemLib
 /**
  * \brief Template class for isoparametric elements
  *
- * \tparam T_ELEMENT        Mesh element class
- * \tparam T_SHAPE          Shape function
- * \tparam T_INTEGRAL       Integration method
- * \tparam T_EXTRAPOLATE    Extrapolation method
+ * \tparam T_ELEMENT            Mesh element class
+ * \tparam T_SHAPE              Shape function
+ * \tparam T_INTEGRAL           Integration method
+ * \tparam T_EXTRAPOLATE        Extrapolation method
+ * \tparam T_NODAL_VECTOR       Nodal vector class
+ * \tparam T_DIM_NODAL_MATRIX   Matrix class for a size of dim * nnodes
+ * \tparam T_DIM_MATRIX         Matrix class for a size of dim * dim
  */
 template <
     class T_MESH_ELEMENT,
     class T_SHAPE,
     class T_INTEGRAL,
-    class T_EXTRAPOLATE
+    class T_EXTRAPOLATE,
+    class T_NODAL_VECTOR,
+    class T_DIM_NODAL_MATRIX,
+    class T_DIM_MATRIX
     >
 class TemplateIsoparametric
 {
 public:
+    typedef T_MESH_ELEMENT MeshElementType;
+    typedef T_SHAPE ShapeFunctionType;
+    typedef T_INTEGRAL IntegrationType;
+    typedef T_EXTRAPOLATE ExtrapolationType;
+    typedef T_NODAL_VECTOR NodalVectorType;
+    typedef T_DIM_NODAL_MATRIX DimNodalMatrixType;
+    typedef T_DIM_MATRIX DimMatrixType;
+    typedef ShapeData<NodalVectorType, DimNodalMatrixType, DimMatrixType> ShapeDataType;
+
     /**
      * Constructor without specifying a mesh element. resetMeshElement() must be called afterwards.
      *
@@ -59,7 +74,7 @@ public:
      * @param e                             Mesh element object
      * @param integration_points_level      the sampling level (default 2)
      */
-    TemplateIsoparametric(const T_MESH_ELEMENT &e, std::size_t integration_points_level=2)
+    TemplateIsoparametric(const MeshElementType &e, std::size_t integration_points_level=2)
     : _ele(&e)
     {
         this->resetMeshElement(e);
@@ -70,17 +85,17 @@ public:
     ~TemplateIsoparametric() {}
 
     /// return current mesh element
-    const T_MESH_ELEMENT* getMeshElement() const {return _ele;}
+    const MeshElementType* getMeshElement() const {return _ele;}
 
     /// reset a mesh element
-    void resetMeshElement(const T_MESH_ELEMENT &e)
+    void resetMeshElement(const MeshElementType &e)
     {
         this->_ele = &e;
         this->_mapping.reset(e);
     }
 
     /// return an integration method
-    const T_INTEGRAL& getIntegrationMethod() const {return _integration;}
+    const IntegrationType& getIntegrationMethod() const {return _integration;}
 
     /**
      * compute shape functions
@@ -88,28 +103,29 @@ public:
      * @param x         point in natural coordinates
      * @param shape     evaluated shape data
      */
-    void computeShapeFunctions(const double *x, ShapeData &shape) const
+    void computeShapeFunctions(const double *x, ShapeDataType &shape) const
     {
         _mapping.computeMappingMatrices(x, shape);
     }
 
     /// make interpolation from nodal values
-    double interpolate(const ShapeData& shape, MathLib::LocalVector &nodal_values) const
+    double interpolate(const ShapeDataType& shape, NodalVectorType &nodal_values) const
     {
         assert(nodal_values.size()==this->_ele->getNNodes());
-        return shape.N.dot(nodal_values);
+        return shape.N.transpose().dot(nodal_values);
     }
 
     /// extrapolate integration point values to nodal values
-    void extrapolate(const MathLib::LocalVector &gp_values, MathLib::LocalVector &nodal_values) const
+    template <class T_VEC>
+    void extrapolate(const T_VEC &gp_values, NodalVectorType &nodal_values) const
     {
-        T_EXTRAPOLATE::extrapolate(gp_values, nodal_values);
+        ExtrapolationType::extrapolate(gp_values, nodal_values);
     }
 
 private:
-    const T_MESH_ELEMENT* _ele;
-    NaturalCoordinatesMapping<T_SHAPE> _mapping;
-    T_INTEGRAL _integration;
+    const MeshElementType* _ele;
+    NaturalCoordinatesMapping<MeshElementType, ShapeFunctionType, ShapeDataType> _mapping;
+    IntegrationType _integration;
 };
 
 } //end
