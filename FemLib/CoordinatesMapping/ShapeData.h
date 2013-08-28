0480499dea722b58cacad660ec7e1175877b1332
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2013-08-28 18:38:09 +0200

    make ShapeData independet of particular matrix type

--- FemLib/CoordinatesMapping/ShapeData.h
+++ FemLib/CoordinatesMapping/ShapeData.h
@@ -16,8 +16,6 @@
 #ifndef SHAPEDATA_H_
 #define SHAPEDATA_H_
 
-#include "MathLib/DataType.h"
-
 namespace FemLib
 {
 
@@ -26,22 +24,27 @@ namespace FemLib
  *
  * Mapping is done between physical coordinates (x,y,z) and natural coordinates (r,s,t)
  */
+template <class T_N, class T_DN, class T_J>
 struct ShapeData
 {
+    typedef T_N ShapeType;
+    typedef T_DN DShapeType;
+    typedef T_J JacobianType;
+
     /// shape function N(r)
-    MathLib::LocalRowVector N;
+    ShapeType N;
 
     /// gradient of shape functions, dN(r)/dr
-    MathLib::LocalMatrix dNdr;
+    DShapeType dNdr;
 
     /// gradient of shape functions, dN(r)/dx
-    MathLib::LocalMatrix dNdx;
+    DShapeType dNdx;
 
     /// Jacobian matrix, J=dx/dr
-    MathLib::LocalMatrix J;
+    JacobianType J;
 
     /// inverse of the Jacobian
-    MathLib::LocalMatrix invJ;
+    JacobianType invJ;
 
     /// determinant of the Jacobian
     double detJ;
@@ -52,9 +55,9 @@ struct ShapeData
      * @param n_nodes   The number of element nodes
      */
     ShapeData(std::size_t dim, std::size_t n_nodes)
-    : N(MathLib::LocalRowVector::Zero(n_nodes)), dNdr(MathLib::LocalMatrix::Zero(dim, n_nodes)),
-      dNdx(MathLib::LocalMatrix::Zero(dim, n_nodes)), J(MathLib::LocalMatrix::Zero(dim, dim)),
-      invJ(MathLib::LocalMatrix::Zero(dim, dim)), detJ(.0)
+    : N(n_nodes), dNdr(dim, n_nodes),
+      dNdx(dim, n_nodes), J(dim, dim),
+      invJ(dim, dim), detJ(.0)
     {}
 
     ~ShapeData() {}
@@ -70,12 +73,13 @@ struct ShapeData
     }
 
 private:
-    void setZero(MathLib::LocalMatrix &mat)
+    template<class T>
+    void setZero(T &mat)
     {
         mat.setZero(mat.rows(), mat.cols());
     }
 
-    void setZero(MathLib::LocalRowVector &vec)
+    void setZero(ShapeType &vec)
     {
         vec.setZero(vec.size());
     }
