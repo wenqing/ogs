7e9485f7a83d4efe5ef53059e129437f02d1cc26
Wenqing Wang <wenqing.wang@ufz.de>
2017-10-18 17:51:53 +0200

    [Coupling] Removed the coupling calculation from the classes of single process

--- ProcessLib/LiquidFlow/LiquidFlowProcess.h
+++ ProcessLib/LiquidFlow/LiquidFlowProcess.h
@@ -83,13 +83,6 @@ public:
         return _gravitational_acceleration;
     }
 
-    LiquidFlowMaterialProperties* getLiquidFlowMaterialProperties() const
-    {
-        return _material_properties.get();
-    }
-
-    void setCoupledSolutionsForStaggeredSchemeToLocalAssemblers() override;
-
 private:
     void initializeConcreteProcess(
         NumLib::LocalToGlobalIndexMap const& dof_table,
