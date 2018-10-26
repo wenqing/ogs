1fcbf73292ada858bffcf9ec662c563a7157ddcf
Dmitry Yu. Naumov <github@naumov.de>
2018-10-26 15:44:12 +0200

    DBG info for pressure update. Move nl_itr incr.

--- ProcessLib/PhaseField/PhaseFieldProcess.cpp
+++ ProcessLib/PhaseField/PhaseFieldProcess.cpp
@@ -314,8 +314,6 @@ void PhaseFieldProcess<DisplacementDim>::postNonLinearSolverConcreteProcess(
         return;
     }
 
-    _process_data.nl_itr++;
-
     std::vector<std::reference_wrapper<NumLib::LocalToGlobalIndexMap>>
         dof_tables;
 
@@ -338,6 +336,7 @@ void PhaseFieldProcess<DisplacementDim>::postNonLinearSolverConcreteProcess(
 #endif
     INFO("Integral of crack: %g", _process_data.crack_volume);
 
+    DBUG("PostNonLinearSolver update pressure.");
     if (!_process_data.propagating_crack)
     {
         return;
@@ -354,6 +353,8 @@ void PhaseFieldProcess<DisplacementDim>::postNonLinearSolverConcreteProcess(
     double const cvol_n = _process_data.crack_volume_n * p_n;
     double const cvol_nm1 = _process_data.crack_volume_nm1 * p_nm1;
     double const vol = _process_data.injected_volume;
+
+    _process_data.nl_itr++;
 
     if (_process_data.pressure < epsilon ||
         std::fabs((cvol_n - cvol_nm1)) < epsilon || _process_data.nl_itr == 1 ||
